0f95009c88fdd09333a7ca6c5d278f2238b8a164
Arvin <arvin.hermit@gmail.com>
2021-02-23 10:46:27 +0800

    init commit

---
+++ src/ConfigProvider.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Php\MongoDb;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+            ],
+            'commands' => [
+            ],
+            'listeners' => [],
+            // 合并到  config/autoload/annotations.php 文件
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => 'The config of mongodb client.',
+                    'source' => __DIR__ . '/../publish/mongodb.php',
+                    'destination' => BASE_PATH . '/config/autoload/mongodb.php',
+                ],
+            ],
+        ];
+    }
+}
